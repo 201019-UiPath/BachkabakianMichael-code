3c361127c7cd468f2c1dbb246e8fab45d78527ac
Michael <Michael.Bachkabakian.995@my.csun.edu>
2020-10-21 05:51:03 -0700

    day 2 work hero app

---
+++ HerosApp/HerosLib/Hero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HerosLib
+{
+    public class Hero
+    {
+       public int id;//value type => System.Int32
+
+       public string name;//reference type
+
+       //constructor
+       public Hero(){
+            id=1;
+            name="Bombasto";
+       }
+
+       public Hero(int id, string name)
+       {
+            this.id=id;
+            this.name=name;
+       }
+       
+           
+       
+
+
+    }
+}
---
+++ HerosApp/HerosUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HerosLib;
+
+namespace HerosUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region default constructor
+            /*Hero obj = new Hero();
+            Console.WriteLine($"{obj.id} {obj.name}");*/
+            #endregion
+            #region Parameterized constructor
+            Hero obj1 = new Hero(2, "Narco");
+            Console.WriteLine($"{obj1.id} {obj1.name}");
+            #endregion
+        }
+    }
+}
