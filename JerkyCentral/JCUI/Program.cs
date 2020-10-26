300cfbf36fef89f98ebd475693a3b1f4831af19b
Michael <Michael.Bachkabakian.995@my.csun.edu>
2020-10-25 22:18:46 -0700

    figured out beginning of algorithm and started user class

--- JerkyCentral/JCUI/Program.cs
+++ JerkyCentral/JCUI/Program.cs
@@ -6,9 +6,33 @@ namespace JCUI
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //0. Create user obj with constructor from user class 
             User customer = new User();
-            System.Console.WriteLine($"User info is {customer.FName} {customer.LName}");
+            
+            //1. Welcome message & Prompt user for login info (Email & PW)
+            /*System.Console.WriteLine("Welcome to Jerky Central, please enter your email address: ");
+            Console.ReadLine();
+
+            **code that checks if input matches a existing email  in the list in db
+            
+            System.Console.WriteLine("Please enter your Password: ");
+            Console.ReadLine();
+
+            **code that checks if input is the correct pw for that email
+
+            */
+
+            //2. Opens either customer menu or manager menu based on users manager status value.
+
+
+            //3. all the menu stuff next
+            
+
+
+            
+
+            
+           
         }
     }
 }
--- JerkyCentral/UserLib/User.cs
+++ JerkyCentral/UserLib/User.cs
@@ -1,20 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UserLib
 {
-    public class User
-    {
-        public string FName;
-        public string LName;
+    public class User{
 
-        //constructor
+        public static List<string> names = new List<string>();
+
+
         public User()
         {
-            FName = "Michael";
-            LName = "Bach";
+            names.Add("joe");
+            names.Add("bill");
+            names.Add("rob");
+            names.Add("sam");
+        }
+        public static List<string> GetNames()
+        {
+            return names;
         }
 
+        public static void RemoveNames(string name)
+        {
+            if(names.Contains(name))
+            {
+                names.Remove(name);
+            } 
 
+        }
+        #region code to prompt user for a name to remove, and to remove it from the names list (goes in the main method)
+        /*System.Console.WriteLine("Please enter a name to remove: ");
+            string nm = Console.ReadLine();
+            User.RemoveNames(nm);
 
+            System.Console.WriteLine();
+
+            foreach(var name in User.names)
+            {
+                System.Console.WriteLine(name);
+
+            }
+            */
+        #endregion
     }
+    
 }
