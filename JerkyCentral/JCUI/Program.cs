111a5628c0f93b43f2e0574bec7a37a5fb210a5c
Michael <Michael.Bachkabakian.995@my.csun.edu>
2020-10-30 18:59:58 -0700

    added the remaining properties and some methods to user class

--- JerkyCentral/JCUI/Program.cs
+++ JerkyCentral/JCUI/Program.cs
@@ -8,17 +8,33 @@ namespace JCUI
     {
         static void Main(string[] args)
         {   //0. Create user obj with constructor from user class 
-            // User customer = new User();
+            User customer = new User();
 
             // System.Console.WriteLine("add some1");
             // string test = Console.ReadLine();
             // User.AddNames(test);
 
-            // foreach(var names in User.GetNames()){
-            //     System.Console.WriteLine(names);
-            // }
-            IMenu startMenu = new CustomerMenu();
-            startMenu.Start();
+            foreach(var names in User.GetNames()){
+                System.Console.WriteLine(names);
+            }
+
+            foreach(var emails in User.GetEmails()){
+                System.Console.WriteLine(emails);
+            }
+
+            foreach(var passwords in User.GetPasswords()){
+                System.Console.WriteLine(passwords);
+            }
+
+            foreach(var managerstatus in User.GetManagerStatus()){
+                System.Console.WriteLine(managerstatus);
+            }
+
+            System.Console.WriteLine("Which users info would you like? joe[0], bill[1], rob[2], sam[3]");
+            int test = Convert.ToInt32(Console.ReadLine());
+            User.GetUserRecord(test);
+            // IMenu startMenu = new CustomerMenu();
+            // startMenu.Start();
             
             //1. Welcome message & Prompt user for login info (Email & PW)
             /*System.Console.WriteLine("Welcome to Jerky Central, please enter your email address: ");
--- JerkyCentral/UserLib/User.cs
+++ JerkyCentral/UserLib/User.cs
@@ -7,6 +7,11 @@ namespace UserLib
 
         public static List<string> names = new List<string>();
 
+        public static List<string> emails = new List<string>();
+
+        public static List<string> passwords = new List<string>();
+
+        public static List<bool> managerstatus = new List<bool>();
 
         public User()
         {
@@ -14,10 +19,41 @@ namespace UserLib
             names.Add("bill");
             names.Add("rob");
             names.Add("sam");
+
+            emails.Add("joe@yahoo.com");
+            emails.Add("bill@gmail.com");
+            emails.Add("rob@hotmail.com");
+            emails.Add("sam@revature.net");
+
+            passwords.Add("mypassword123");
+            passwords.Add("dontforget1");
+            passwords.Add("qazwsx999");
+            passwords.Add("bingbangboom11");
+
+            managerstatus.Add(false);
+            managerstatus.Add(true);
+            managerstatus.Add(true);
+            managerstatus.Add(true); 
+
         }
         public static List<string> GetNames()
         {
             return names;
+        }
+
+        public static List<string> GetEmails()
+        {
+            return emails;
+        }
+
+        public static List<string> GetPasswords()
+        {
+            return passwords;
+        }
+
+        public static List<bool> GetManagerStatus()
+        {
+            return managerstatus;
         }
 
         public static void AddNames(string name)
@@ -36,6 +72,52 @@ namespace UserLib
             {
                 names.Remove(name);
             } 
+
+        }
+
+        public static void AddEmails(string email)
+        {
+            if(emails.Contains(email))
+            {
+                System.Console.WriteLine("That Email Address is already in the Database");
+            } else
+            {
+                emails.Add(email);
+            }
+        }
+        public static void RemoveEmails(string email)
+        {
+            if(emails.Contains(email))
+            {
+                emails.Remove(email);
+            } 
+
+        }
+
+        public static void AddPasswords(string password)
+        {
+            if(passwords.Contains(password))
+            {
+                System.Console.WriteLine("That Password is already in the Database");
+            } else
+            {
+                passwords.Add(password);
+            }
+        }
+        public static void RemovePasswords(string password)
+        {
+            if(passwords.Contains(password))
+            {
+                passwords.Remove(password);
+            } 
+
+        }
+
+        //get customers entire customer record 
+        public static void GetUserRecord(int listindex)
+        {
+
+            System.Console.WriteLine($"{names[listindex]} {emails[listindex]} {passwords[listindex]} {managerstatus[listindex]}");
 
         }
         #region code to prompt user for a name to remove, and to remove it from the names list (goes in the main method)
