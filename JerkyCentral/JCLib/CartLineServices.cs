44c68a92c99f5ff228c7661d413298515179f819
Michael <Michael.Bachkabakian.995@my.csun.edu>
2020-11-02 22:04:22 -0800

    filled in ProductServices class and added and using statements to rest of service classes in JCLib

--- JerkyCentral/JCLib/CartLineServices.cs
+++ JerkyCentral/JCLib/CartLineServices.cs
@@ -1,3 +1,9 @@
+using JCDB;
+using JCDB.Models;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
 namespace JCLib
 {
     public class CartLineServices
--- JerkyCentral/JCLib/ProductServices.cs
+++ JerkyCentral/JCLib/ProductServices.cs
@@ -1,7 +1,44 @@
+using JCDB;
+using JCDB.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
 namespace JCLib
 {
     public class ProductServices
     {
-        
+        private IProductRepo repo;
+
+        public ProductServices(IProductRepo repo)
+        {
+            this.repo = repo;
+        }
+        public void AddProduct(Product product)
+        {
+            repo.AddProductAsync(product);
+        }
+        public void UpdateProduct(Product product)
+        {
+            repo.UpdateProduct(product);
+        }
+        public void DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+        }
+        public Product GetProductById(int id)
+        {
+            Product product = repo.GetProductById(id);
+            return product;
+        }
+        public Product GetProductByName(string name)
+        {
+            Product product = repo.GetProductByName(name);
+            return product;
+        }
+        public Task<List<Product>> GetAllProducts()
+        {
+            Task<List<Product>> products = repo.GetAllProductsAsync();
+            return products;
+        }
     }
 }
