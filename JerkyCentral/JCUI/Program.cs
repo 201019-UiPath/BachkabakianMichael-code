d2bf42df5870cc9456591962a1214ff17535de1e
Michael <Michael.Bachkabakian.995@my.csun.edu>
2020-11-03 21:10:20 -0800

    minor change in UI

--- JerkyCentral/JCUI/Program.cs
+++ JerkyCentral/JCUI/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using JCDB.Models;
+﻿using JCUI.Menus;
+using JCDB;
 
 
 namespace JCUI
@@ -7,51 +7,11 @@ namespace JCUI
     class Program
     {
         static void Main(string[] args)
-        {   //0. Create user obj with constructor from user class 
-            User customer = new User();
-            System.Console.WriteLine("hi");
-            
-            // System.Console.WriteLine("add some1");
-            // string test = Console.ReadLine();
-            // User.AddNames(test);
+        {   
+            JCContext context = new JCContext();
 
-            // foreach(var names in User.GetNames()){
-            //     System.Console.WriteLine(names);
-            // }
-
-            // foreach(var emails in User.GetEmails()){
-            //     System.Console.WriteLine(emails);
-            // }
-
-            // foreach(var passwords in User.GetPasswords()){
-            //     System.Console.WriteLine(passwords);
-            // }
-
-            // foreach(var managerstatus in User.GetManagerStatus()){
-            //     System.Console.WriteLine(managerstatus);
-            // }
-
-            // System.Console.WriteLine("Which users info would you like? joe[0], bill[1], rob[2], sam[3]");
-            // int test = Convert.ToInt32(Console.ReadLine());
-            // User.GetUserRecord(test);
-
-
-            // IMenu startMenu = new CustomerMenu();
-            // startMenu.Start();
-            
-            //1. Welcome message & Prompt user for login info (Email & PW)
-            
-
-            //2. Opens either customer menu or manager menu based on users manager status value.
-
-
-            //3. all the menu stuff next
-            
-
-
-            
-
-            
+            IMenu LoginMenu = new LoginMenu(context, new DBRepo(context), new DBRepo(context), new DBRepo(context));
+            LoginMenu.Start();       
            
         }
     }
